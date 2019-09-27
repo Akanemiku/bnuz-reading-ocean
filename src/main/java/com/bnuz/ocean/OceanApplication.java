df57431694c54513b7266d20c78c80a3c2100a15
akane <806394913@qq.com>
2019-09-27 16:48:44 +0800

    -init-

---
+++ src/main/java/com/bnuz/ocean/OceanApplication.java
@@ -0,0 +1,13 @@
+package com.bnuz.ocean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceanApplication.class, args);
+    }
+
+}
